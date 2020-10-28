4e3da659c569a3d25350d511e4600bd096bd6202
mushfiqurmahi <mushfiqurmahi2003@gmail.com>
2020-10-28 14:43:19 +0600

    prime finder working

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
-const VIEW = new URLSearchParams(window.location.search).get('v');
-const INPUT = new URLSearchParams(window.location.search).get('i');
+class InputValidation{
+    constructor(input_string){
+        //100
+        //1-100
+        //1-100-10
+        this.input = [];
 
-function inputVal(val) {
-    console.log("[TAKING INPUT]");
-    let nums = val.split('-');
+        let input = input_string.split('-').map(e => parseInt(e));
+        if(input.length === 0) throw new Error('[NO INPUT]');
+        if(input.length === 1) this.input = [1, input[0], 1];
+        else if(input.length === 2) this.input = [...input , 1];
+        else if(input.length === 3) this.input = input;
+        else throw new Error('[INVALID INPUT]');
 
-    console.log("[INPUT]", nums);
-
-    if (nums.length === 1) {
-        // he just passed only one number
-        calculate(1, nums[0]) // 1 is not prime (starting from 1)
-    }
-    else if (nums.length === 2) {
-        // start-end
-        calculate(...nums)
-    }
-    else if (nums.length === 3) {
-        // start-end-step
-        calculate(...nums)
-    }
-    else {
-        // wrong input
-        console.error("[WRONG INPUT]");
-        document.body.innerHTML = '<p>Invalid Arguments<p>';
-        return;
+        console.log('[INPUT:InputValidation]', this.input)
     }
 }
 
-class CalculatePrime {
-    constructor(_end) {
+class CalculatePrime extends InputValidation{
+    constructor(input_string) {
+        super(input_string);
+
+        let [start, end, step] = this.input;
+        CalculatePrime.validateInput(start, end, step);
+
         this.all_primes = JSON.parse(localStorage.getItem('all_primes')) || [];
-        this.isAllPrime(_end);
+        CalculatePrime.isAllPrime(end, this.all_primes);
     }
 
-    static isAllPrime(end) {
-        /* This func calculate all prime and mutate the global all_primes variable */
+    static validateInput(start, end, step) {
+        console.log("[VALIDATING]");
+        if (!(start && end && step && start > 0 && end > 0 && step > 0 && start < end)) {
+            //wrong input
+            console.error("[WRONG INPUT]: from CalculatePrime");
+            throw new Error('[WRONG INPUT]');
+        }
+    }
+
+    static isAllPrime(end,  all_primes) {
+        /* This func calculate all primes and mutate the object's all_primes property + set new calculated values in local storage*/
 
         let starting_point = 1; //calculation will be started form this val
-        if (this.all_primes.length) {
+        if (all_primes.length) {
             // if the array is not empty, check how many values has caculated already
 
-            let last_element = this.all_primes[this.all_primes.length - 1];
+            let last_element = all_primes[all_primes.length - 1];
             if (end <= last_element) {
                 // means all values are caculated and no need to count(mutate global var)
                 return;
@@ -52,20 +55,20 @@ class CalculatePrime {
         }
 
         for (let i = starting_point; i <= end; i++) {
-            if (this.isPrime(i)) {
+            if (CalculatePrime.isPrime(i, all_primes)) {
                 // returns bool
                 // list not contains that number
                 // add it to the list
                 //console.info('[new Prime]', i);
-                this.all_primes.push(i);
+                all_primes.push(i);
             }
         }
 
         //after all calculation, store that array  for future use
-        localStorage.setItem('all_primes', JSON.stringify(this.all_primes));
+        localStorage.setItem('all_primes', JSON.stringify(all_primes));
     }
 
-    static isPrime(num) {
+    static isPrime(num, all_primes) {
         // num == integer > 0; already checked!
 
         // '1' may come so check it
@@ -87,110 +90,95 @@ class CalculatePrime {
 
 class PrimeFinder extends CalculatePrime {
 
-    constructor(start, end, step = 1) {
-        // => int
-        this.start = start;
-        this.end = end;
-        this.step = step;
-
-        console.log("[VALIDATING]");
-        this.validateInput();
-
+    constructor(input_string) {
         console.log("[CACULATING]");
-        console.log(start, end, step);
-        super().isAllPrime(end);
+        super(input_string);
+
+        this.total_nums = 0; // total numbers
+        this.needed_primes = []; // total prime numbers' list
+        [this.start, this.end, this.step] = this.input
     }
 
-    validateInput(){
-        if (!(this.start && this.end && this.step && this.start > 0 && this.end > 0 && this.step > 0 && this.start < this.end)) {
-            //wrong input
-            console.error("[WRONG INPUT]: from Primefinder");
-            throw new Error('[WRONG INPUT]');
-        }
-    }
+    updateDom(main_list) {
 
-    updateDom(){
+        let new_array = this.all_primes.slice(); // all_primes not empty here
+        //copying array for efficiency (there are several methods for copying an array)
 
-        console.log('[notShowResult]', notShowRes);
-        // console.log('[showAllNumbers]', showAllNumbers);
-        // console.log('[showPrime]', showPrime);
-    
-        if(notShowRes){
-            // user don't want to show all between start and end;
-            //todo: ad ifPrime here
-            return;
-        }
-        
-        document.querySelector('.result-visual').innerHTML = '';
-        // clearing dom af there was any
-        
-        let new_array = all_primes.slice(); // all_primes not empty here
-        //copying array for efficiency (there are several method for copying an array)
-    
         // delete those primes which are less than starting number
         // terminating "i > new_array[0]" condition
-        while (true){
+        while (true) {
             if (!new_array.length || new_array[0] >= this.start) break;
             // break if array is empty OR array[0] > starting_value
             new_array.shift();
         }
-    
+
         /* here is a problem with step value -solved in the next section
         // after deleting all unnecessary primes, length of the new_array will be the total_primes value
         let total_primes = new_array.length;
         // update dom
         document.querySelector('.totals').innerHTML = total_primes;
         */
-    
-       console.log('[Primes]-[after 1st portion deleted] ', new_array);
-       
-       let total_nums = 0; // all numbers that user needed
-       let elementsToAppend = ""; // innerHtml
-    
-       for (let i=this.start; i<=this.end; i+=this.step){
-           // increase total_nums by one;
-           total_nums++;
-    
-            if ( !new_array.length || i < new_array[0] ){
+
+        console.log('[Primes]-[after 1st portion deleted] ', new_array);
+
+        let innerHTML = "";
+
+        for (let i = this.start; i <= this.end; i += this.step) {
+            // increase total_nums by one;
+            this.total_nums++;
+
+            if (!new_array.length || i < new_array[0]) {
                 /*Checking if ...
                 1. new_array is empty (means no prime left)
                 2. or if not empty check current num is less than 1st element of primes 
                 */
-              
+
                 // insert them as normal* numbers
-           
-                elementsToAppend+=`<div><span>${i}</span></div>`; 
-                continue;
+                innerHTML += `<span>${i}</span>`;
             }
-            else if ( i > new_array[0] ){
+            else if (i > new_array[0]) {
                 new_array.shift();
-                console.log('[Deleted]', new_array.shift())
-            
-                elementsToAppend+=`<div><span>${i}</span></div>`;
-                continue;
+                // console.log('[Deleted]', new_array.shift())
+
+                innerHTML += `<span>${i}</span>`;
             }
             else {
                 // if i==all_primes[0] --> means its a prime number
-                
+
                 /*as we have deleted all i > new_array[0] numbers this condition will never true */
-                
+
                 // insert it as prime*  number
                 // and delete the 1st item(prime) of the array
-    
-                elementsToAppend+=`<div data-prime><span>${i}</span></div>`;
+
+                innerHTML += `<span data-x>${i}</span>`;
                 new_array.shift();
-                needed_primes.push(i);
-                continue;
+                this.needed_primes.push(i);
             }
         }
-        console.log('[Needed Primes]', needed_primes);
-        
+        console.log('[Needed Primes]', this.needed_primes);
+
         // after all update the counts and results in DOM
-        document.querySelector('.totals').innerHTML = total_nums;
-        document.querySelector('.primes').innerHTML = needed_primes.length;
-        document.querySelector('.notPrimes').innerHTML = total_nums - needed_primes.length;
-    
-        document.querySelector('.result-visual').innerHTML = elementsToAppend;
+        main_list.innerHTML = innerHTML;
+        // document.querySelector('.primes').innerHTML = this.needed_primes.length;
+        // document.querySelector('.notPrimes').innerHTML = total_nums - this.needed_primes.length;
     }
 
 }
+
+function main() {
+    const VIEW = new URLSearchParams(window.location.search).get('v');
+    const INPUT = new URLSearchParams(window.location.search).get('i');
+    console.log('[INPUT]:', INPUT)
+
+    try {
+        if (INPUT){
+            let main_list = document.querySelector('.card__result');
+            let obj = new PrimeFinder(INPUT);
+            obj.updateDom(main_list);
+            console.dir(obj)
+        }
+    } catch (e) {
+        console.dir(e);
+    }
+}
+main()
